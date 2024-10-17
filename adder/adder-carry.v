b8a44fc78dfbc843d4fcfc19bdbc52f1ac68a833
jason-fxz <jason341132@qq.com>
2024-10-17 15:16:26 +0800

    实现简单超前进位加法器
    Signed-off-by: jason-fxz <jason341132@qq.com>

--- adder/adder-carry.v
+++ adder/adder-carry.v
@@ -1,28 +1,72 @@
-/* ACM Class System (I) Fall Assignment 1 
- *
- *
- * Implement your naive adder here
- * 
- * GUIDE:
- *   1. Create a RTL project in Vivado
- *   2. Put this file into `Sources'
- *   3. Put `test_adder.v' into `Simulation Sources'
- *   4. Run Behavioral Simulation
- *   5. Make sure to run at least 100 steps during the simulation (usually 100ns)
- *   6. You can see the results in `Tcl console'
- *
- */
+// ref: https://blog.csdn.net/qq_39507748/article/details/108911941
+
+module CLA4 (
+	input [3:0] a,
+	input [3:0] b,
+	input Cin,
+	output [3:0] sum,
+	output Cout
+);
+	wire [3:0] G, P, C;
+	assign P = a ^ b;
+	assign G = a & b;
+
+	assign C[0] = Cin;
+    assign C[1] = G[0] | (P[0] & C[0]);
+    assign C[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);
+    assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
+    assign Cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & C[0]);
+
+	assign sum = P ^ C;
+endmodule
+
+module RCLA16 (
+	input [15:0] a,
+	input [15:0] b,
+	input Cin,
+	output [15:0] sum,
+	output Cout
+);
+	wire [3:0] C;
+	genvar i;
+	generate
+		for (i = 0; i < 4; i = i + 1) begin
+			CLA4 cla4_inst (
+				.a(a[i*4 +: 4]),
+				.b(b[i*4 +: 4]),
+				.Cin(i == 0 ? Cin : C[i-1]),
+				.sum(sum[i*4 +: 4]),
+				.Cout(C[i])
+			);
+		end
+	endgenerate
+	
+	assign Cout = C[3];
+endmodule
+
 
 module adder(
-	// TODO: Write the ports of this module here
-	//
 	// Hint: 
 	//   The module needs 4 ports, 
 	//     the first 2 ports are 16-bit unsigned numbers as the inputs of the adder
 	//     the third port is a 16-bit unsigned number as the output
 	//	   the forth port is a one bit port as the carry flag
 	// 
+	input [15:0] a,
+	input [15:0] b,
+	output [15:0] answer,
+	output carry
 );
-	// TODO: Implement this module here
+
+	RCLA16 rcla16_inst (
+		.a(a),
+		.b(b),
+		.Cin(1'b0),
+		.sum(answer),
+		.Cout(carry)
+	);
+
+	
+
 	
 endmodule
--- adder/adder-ripple.v
+++ adder/adder-ripple.v
@@ -14,15 +14,30 @@
  */
 
 module adder(
-	// TODO: Write the ports of this module here
-	//
 	// Hint: 
 	//   The module needs 4 ports, 
 	//     the first 2 ports are 16-bit unsigned numbers as the inputs of the adder
 	//     the third port is a 16-bit unsigned number as the output
 	//	   the forth port is a one bit port as the carry flag
 	// 
+	input [15:0] a,
+	input [15:0] b,
+	output [15:0] answer,
+	output carry
 );
-	// TODO: Implement this module here
+	wire [16:0] temp; // temp carry
+
+	assign temp[0] = 1'b0;
+
+	genvar i;
+	generate
+		for (i = 0; i < 16; i = i + 1) begin
+			assign answer[i] = a[i] ^ b[i] ^ temp[i];
+			assign temp[i + 1] = (a[i] & b[i]) | (a[i] & temp[i]) | (b[i] & temp[i]);
+		end
+	endgenerate
+
+	assign carry = temp[16];
+
 	
 endmodule
